f791201d4d0dff8a209dd026bd1426bc2dbe8e08
Zack Lundqvist <Zack.lundqvist@hotmail.com>
2019-02-13 13:53:28 +0100

    Client remove redundant files and add nav bar

---
+++ client/src/components/Navigation/NavButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="button">
+    <router-link v-bind:to="to" tag="div">{{ text }}</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavButton",
+  props: {
+    text: String,
+    to: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.button {
+  width: 100px;
+  height: 50px;
+  background-color: #42b983;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+}
+.button:hover {
+   color: white;
+   background-color: #42b983;
+   cursor: default;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  	<Navigation></Navigation>
+  	<div class="content">
+  		<DataBox v-for="box in boxes" v-bind:title="box.title" v-bind:value="box.value" />
+  	</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import DataBox from "@/components/DataBox.vue";
+import Navigation from "@/components/Navigation/Navigation.vue";
+const Web3 = require('web3');
+const web3 = new Web3(Web3.givenProvider);
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    DataBox,
+    Navigation
+  },
+  data () {
+  	return {
+  		boxes: [
+  		{ title: "Network Type", value: null },
+  		{ title: "", value: null },
+  		{ title: "", value: null },
+  		{ title: "", value: null },
+  		{ title: "", value: null },
+  		{ title: "", value: null }]
+  	}
+  },
+  created () {
+  	this.fetchData();
+  },
+  methods: {
+  	fetchData() {
+  		web3.eth.net.getNetworkType().then(type => {
+  			this.boxes[0].value = type;
+  		});
+  	}
   }
 };
 </script>
+
+<style scoped>
+.home {
+	width: 100%;
+}
+.content {
+	display: flex;
+	justify-content: space-between;
+	flex-wrap: wrap;
+	margin-left: 10%;
+	margin-right: 10%;
+}
+</style>
