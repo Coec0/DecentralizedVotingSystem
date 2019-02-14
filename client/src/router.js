130449f62ca1f74b9eaf8b4e565ee15d2cfb8bd5
Zack Lundqvist <Zack.lundqvist@hotmail.com>
2019-02-14 08:29:42 +0100

    Client Add admin route

--- client/src/router.js
+++ client/src/router.js
@@ -12,6 +12,11 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+    },
+    {
+      path: "/admin",
+      name: "admin",
+      component: Admin
     }
   ]
 });
