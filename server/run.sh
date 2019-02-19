43b05f9524836022755e3f792a0fb5e288f8cd0b
Latiif <llusx@outlook.com>
2019-02-19 23:54:20 +0100

    add check for necessary software in run script

--- server/run.sh
+++ server/run.sh
@@ -1,6 +1,19 @@
-echo "Compiling the code.."
+echo "Checking for necessary programs ..."
+
+if ! [ -x "$(command -v mvn)" ]; then
+  echo 'Error: maven is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v java)" ]; then
+  echo 'Error: java is not installed.' >&2
+  exit 1
+fi
+
+clear
+echo "Compiling the code..."
 mvn clean install
 clear
-echo -e "Done!\n Running the server at port 8080"
+echo -e "Done!\nRunning the server at port 8080"
 cd target
 java -jar Server-0.6-jar-with-dependencies.jar
