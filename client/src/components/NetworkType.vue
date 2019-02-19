908176d9c1c1fcbdac39c5be0074fab192fc376b
Zack Lundqvist <Zack.lundqvist@hotmail.com>
2019-02-19 22:55:31 +0100

    Add network status to store

--- client/src/components/NetworkType.vue
+++ client/src/components/NetworkType.vue
@@ -1,39 +1,41 @@
 <template>
 	<div class="network-type">
-		{{ type }}
+		{{ status }}
 	</div>
 </template>
 
 <script>
 export default {
 	name: 'NetworkType',
-	data() {
-		return {
-			type: null
-		};
-	},
 	created() {
 		this.updateStatus();
 
-		setInterval(function () {
+		setInterval(function() {
 			this.updateStatus();
 		}.bind(this), 2000);
 	},
 	methods: {
 		updateStatus() {
-			if (!this.$store.state.web3) {
-				this.type = 'Not connected (Web3 not initialized)';
+			if (!this.$store.state.network.web3) {
+				this.$store.commit('setNetworkStatus', 'Not connected (Web3 not initialized)');
 				return;
 			}
 
-			if (!this.$store.state.web3.currentProvider) {
-				this.type = 'Not connected (No provider)';
+			if (!this.$store.state.network.web3.currentProvider) {
+				this.$store.commit('setNetworkStatus', 'Not connected (No provider)');
 				return;
 			}
 
-			this.$store.state.web3.eth.net.getNetworkType().then(t => {
-				this.type = `Connected to ${t} network (${this.$store.state.web3.currentProvider.host})`;
+			this.$store.state.network.web3.eth.net.getNetworkType().then(t => {
+				this.$store.commit('setNetworkStatus', `Connected to ${t} network (${this.$store.state.network.web3.currentProvider.host})`);
+			}).catch((error) => {
+				this.$store.commit('setNetworkStatus', `Could not connect to network (${this.$store.state.network.web3.currentProvider.host})`);
 			});
+		},
+	},
+	computed: {
+		status() {
+			return this.$store.state.network.status;
 		}
 	}
 };
@@ -43,11 +45,10 @@ export default {
 <style scoped>
 .network-type {
 	position: fixed; /* Sit on top of the page content */
-	display: block; /* Hidden by default */
+	display: block;
 	right: 0;
 	bottom: 0;
 	z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
-	cursor: default; /* Add a pointer on hover */
 	color: black;
 }
 </style>
--- client/src/store.js
+++ client/src/store.js
@@ -5,21 +5,39 @@ const axios = require('axios');
 
 Vue.use(Vuex);
 
-console.log(process.env)
+const dev = process.env.NODE_ENV === 'development';
 
 const axiosInstance = axios.create({
-	baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api' : 'http://URL.com/api',
-	timeout: 1000
+	baseURL: 'http://localhost:8080/api',
+	timeout: 5000
 });
 
 export default new Vuex.Store({
 	state: {
-		$web3: null, //$ prefix is to disable Vue from attaching observers to web3, which yields an error
+		dev: dev,
+		network: {
+			$web3: null, //$ prefix is to disable Vue from attaching observers to web3, which results in an error
+			status: 'Retreiving status...'
+		},
 		axios: axiosInstance
 	},
 	mutations: {
 		setWeb3Provider(state, provider) {
-			state.web3 = new Web3(provider);
+			if(state.network.web3) {
+				state.network.web3.setProvider(provider);
+			} else {
+				state.network.web3 = new Web3(provider);
+			}
+		},
+		setWeb3HttpProvider(state, provider) {
+			if(state.network.web3) {
+				state.network.web3.setProvider(new Web3.providers.HttpProvider(provider));
+			} else {
+				state.network.web3 = new Web3(new Web3.providers.HttpProvider(provider));
+			}
+		},
+		setNetworkStatus(state, status) {
+			state.network.status = status;
 		}
 	}
 });
