b2acafc7e0ff7dcf09fd0ef024407fcfd1bd95ba
Coec0 <oscca@student.chalmers.se>
2019-02-19 15:20:53 +0100

    Added comments to functions

--- votingsystem/votingsystem.sol
+++ votingsystem/votingsystem.sol
@@ -21,7 +21,7 @@ contract VotingSystem {
     mapping(uint => uint) public idToIndexMap; 
     //mapping(address => uint) public votesMap; //Voters address map what candidate it voted for
     bool enableWhitelist = false;
-    uint private candidateArrayPosition; //Integer that keeps track of positions occupied by candidates in allCantidates[]
+    //uint private candidateArrayPosition; //Integer that keeps track of positions occupied by candidates in allCantidates[]
 
     constructor(bytes32[] memory candidates, uint blockamount) public{ //blockamount == amount of blocks
     
@@ -30,19 +30,22 @@ contract VotingSystem {
         blockStopNumber = blockamount + block.number;
 
         //Add BlankVote
-        createCandidate(0x426c616e6b566f74650000000000000000000000000000000000000000000000);
+        addCandidate(0x426c616e6b566f74650000000000000000000000000000000000000000000000);
 
+        //Adds all canidates from the constructor
         for(uint i=0; i < candidates.length; i++){
-            createCandidate(candidates[i]);
+            addCandidate(candidates[i]);
         }
         
     }
     
-    function createCandidate(bytes32 candidate) private{
+    //Creates a candidate from the name of the candidate and adds it to allCandidates[].
+    function addCandidate(bytes32 candidate) public{
+        require(isVotingOpen(), "Voting is closed!");
         require(candidate != NOT_INSTANTIATED, "A candidate may not have 0x00.. as name");
-        uint hash = uint( keccak256(abi.encodePacked(candidate,candidateArrayPosition)));
+        uint hash = uint( keccak256(abi.encodePacked(candidate,allCandidates.length)));
         
-        idToIndexMap[hash] = candidateArrayPosition++;
+        idToIndexMap[hash] = allCandidates.length;
             
         allCandidates.push(Candidate({
               id: hash,
@@ -51,14 +54,20 @@ contract VotingSystem {
         }));
     }
     
+    //Adds a voter to the whitelist (röstlängd), allowing it to vote.
+    //If no voter is added, the whitelist is disabled
     function addVoterToWhitelist(address adr) public {
         enableWhitelist = true; //TODO This is just for eaisier testing
         voterMap[adr].whitelisted = true;
     }
+    
+    //Removes a voter from the whitelist(röstlängd), disallowing it to vote
     function removeVoterFromWhitelist(address adr) public{
         voterMap[adr].whitelisted = false;
     }
     
+    //Adds some accounts to the whitelist. Theese accounts are the first three
+    //accounts in the "remix" IDE.
     function debugAddTestWhitelistVoters() public {
         //Add some default accounts that are allowed to vote:
         voterMap[0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c].whitelisted = true;
@@ -68,17 +77,20 @@ contract VotingSystem {
         enableWhitelist = true;
     }
 
+    //Returns the amounts of blocks left until the vote is over
     function blocksLeft () public view returns (uint){
          return blockStopNumber - block.number;
     }
 
+    //Checks if the voting is open
     function isVotingOpen () public view returns (bool){
         if(blocksLeft() <= 0)
             return false;
         return true;
     }
     
-    //Checks if an address is on the whitelist
+    //Checks if an address is on the whitelist. If the whitelist
+    //isn't enabled then always returns true
     function isOnWhitelist(address adr) private view returns (bool){
         if(!enableWhitelist){ return true;} //For easy debugging
         
@@ -86,6 +98,7 @@ contract VotingSystem {
     }
 
     //from ethereum.stackexchange.com. Author ismael
+    //Should only be used for debugging
     function bytes32ToString (bytes32 data) internal pure returns (string memory) {
         bytes memory bytesString = new bytes(32);
         for (uint j=0; j<32; j++) {
@@ -98,7 +111,8 @@ contract VotingSystem {
     }
 
     //with pure you cannot access the contract storage
-    function getCandidateStringNameIdx(uint index) public view returns (string memory){
+    //Gets the candidate string name from index in array position
+    function debugGetCandidateStringNameIdx(uint index) public view returns (string memory){
         if(index >= allCandidates.length){
             return "Candidate not found";
         }
@@ -107,28 +121,20 @@ contract VotingSystem {
         return bytes32ToString(c.name);
     }
 
-    function addCandidate(bytes32 _name) public{
-        require(isVotingOpen(), "Voting is closed!");
-        uint hash = uint( keccak256(abi.encodePacked(_name,allCandidates.length)));
-        idToIndexMap[hash] = allCandidates.length;
-        allCandidates.push(Candidate({
-                id: hash,
-                name: _name,
-                votecount: 0
-            }));
-    }
-
-    function getCandidateStringNameID(uint id) public view returns (string memory){
+    //Get candidate string name from its ID.
+    function debugGetCandidateStringNameID(uint id) public view returns (string memory){
         if(!doesCandidateExist(id)){
             return "Candidate not found";
         }
-        uint index = getCandidateIndex(id);
+        uint index = debugGetCandidateIndex(id);
         Candidate memory c = allCandidates[index];
         return bytes32ToString(c.name);
     }
 
-    function getCandidateIndex(uint id) private view returns (uint idx){
-        require(doesCandidateExist(id),"Error candite doesent exist");
+
+    //Helper function for debugGetCandidateStringNameID
+    function debugGetCandidateIndex(uint id) private view returns (uint idx){
+        require(doesCandidateExist(id),"Error candidate doesent exist");
         for(uint i = 0; i < allCandidates.length ; i++){
             if(allCandidates[i].id == id){
                 idx = i;
@@ -136,6 +142,7 @@ contract VotingSystem {
         }
     }
 
+    //Checks if a candidate exists
     function doesCandidateExist (uint id) private view returns (bool isValid){
         isValid = false;
         for(uint i = 0; i < allCandidates.length ; i++){
@@ -146,6 +153,7 @@ contract VotingSystem {
     }
 
     //Currently Loops through all candidates (o(n)).
+    //Get the candidate thats in lead
     function getCandidateInLead() public view returns
         (uint id, bytes32 name, uint votes){
             for(uint i = 0; i < allCandidates.length ; i++){
@@ -160,6 +168,7 @@ contract VotingSystem {
     //msg.sender is the address of person or
     //other contract that is interacting with
     //contract right now
+    //This function votes
     function vote (uint id) public {
         require(isVotingOpen(), "Voting is closed!");
         require(doesCandidateExist(id), "Not a valid ID");
