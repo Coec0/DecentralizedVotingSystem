bb44b9c63a98b87c0e6a6afef822144c357ff79c
Zack Lundqvist <Zack.lundqvist@hotmail.com>
2019-02-21 13:58:31 +0100

    Add deploy functionality

--- compile-deploy-smartcontract/src/app.js
+++ compile-deploy-smartcontract/src/app.js
@@ -1,25 +1,65 @@
 const path = require('path');
 const fs = require('fs-extra');
 const solc = require('solc');
-const web3 = require('web3');
+const Web3 = require('web3');
 
 const buildPath = path.resolve('build');
 const contractsPath = path.resolve('contracts');
 
-//Check args
-let errors = [];
-if(!process.argv[2]) {
-	errors.push('> No ethereum node specified in first argument');
-}
-if(!process.argv[3]) {
-	errors.push('> No private key specified as second argument');
-}
-if(errors.length > 0) {
-	console.error('Error(s) detected!');
-	errors.forEach(e => console.error(e));
-	return;
-}
+var web3 = null;
+var account = null;
 
+const checkPrerequisits = function() {
+	return new Promise(async (resolve, reject) => {
+		//Check args
+		let errors = [];
+		if(!process.argv[2]) {
+			errors.push('No ethereum node specified in first argument');
+		} else {
+			try {
+				console.log('Connecting to node...');
+				web3 = new Web3(process.argv[2]);
+				let timeout = new Promise((resolve, reject) => {
+					let wait = setTimeout(() => {
+						resolve('timeout');
+					}, 4000)
+				});
+				let type = web3.eth.net.getNetworkType();
+				let result = await Promise.race([timeout, type]);
+
+				if(result == 'timeout') {
+					errors.push('Timed out trying to connect to node ' + process.argv[2]);
+					reject(errors);
+					return;
+				}
+
+				console.log(`Successfully connected to node ${process.argv[2]} of type ${result}`);
+			} catch(err) {
+				errors.push(err.message);
+			}
+		}
+		if(!process.argv[3]) {
+			errors.push('No private key specified as second argument');
+		} else {
+			try {
+				if(!process.argv[3].startsWith('0x')) {
+					errors.push('Invalid private key (Must be begin with "0x"): ' + process.argv[3]);
+				} else {
+					account = web3.eth.accounts.privateKeyToAccount(process.argv[3]);
+					console.log(`Account retrieved: ${account.address}`);
+					let balance = await web3.eth.getBalance(account.address);
+					console.log(`Account balance: ${balance} ETH`);
+					resolve();
+				}	
+			} catch (err) {
+				errors.push(err.message);
+			}
+		}
+		if(errors.length > 0) {
+			reject(errors);
+		}
+	});
+}
 
 const createBuildFolder = () => {
 	fs.emptyDirSync(buildPath);
@@ -58,18 +98,56 @@ const compileContracts = function() {
 		return;
 	}
 
-	console.time('Compilation');
+	console.time('Compiled in');
 	const compiledContracts = JSON.parse(solc.compile(JSON.stringify(input))).contracts;
 
 	for (let contract in compiledContracts) {
 		for(let contractName in compiledContracts[contract]) {
 			fs.outputJsonSync(path.resolve(buildPath, `${contractName}.json`), compiledContracts[contract][contractName], { spaces: '\t' });
-			console.log(`Compiled ${contractName}.json`);
+			console.log(`Compiled ${contractName}.sol to ${contractName}.json`);
 		}
 	}
 
-	console.timeEnd('Compilation');
+	console.timeEnd('Compiled in');
 }
 
-createBuildFolder();
-compileContracts();
+const deploy = function() {
+	return new Promise((resolve, reject) => {
+		const compiledFiles = fs.readdirSync(buildPath);
+
+		compiledFiles.forEach(async (file) => {
+			console.log(`Attempting to deploy contract ${file} from account: ${account.address}`);
+
+			const filePath = path.resolve(buildPath, file);
+			const contract = require(filePath);
+
+			try  {
+				const deployedContract = await new web3.eth.Contract(contract.abi)
+				.deploy({
+					data: '0x' + contract.evm.bytecode.object,
+					arguments: process.argv[4]
+				})
+				.send({
+					from: account,
+					gas: '2000000'
+				});
+
+				console.log(`Contract ${file} deployed at address: ${deployedContract.options.address}`);
+			} catch (err) {
+				console.log(`Error deploying contract ${file}. ${err.message}`);
+			}
+		});
+	});
+};
+
+checkPrerequisits().then(() => {
+	createBuildFolder();
+	compileContracts();
+	deploy().then(() => {
+		process.exit(0);
+	});
+}).catch((errors) => {
+	console.error('Error(s) detected!');
+	errors.forEach(e => console.error('> ' + e));
+	process.exit(1);
+});
--- deploy-contract.sh
+++ deploy-contract.sh
@@ -1,9 +1,11 @@
 NODE="ws://localhost:7545"
-PRIVATE_KEY=""
+PRIVATE_KEY="0xed75458862753c6cb536343a4e7d9cb3e3fffc5d0069720e0489c7cb27bafb9e"
+CONTRACT_ARGS=""
 
 cd votingsystem
 find -iname '*.sol' -exec cp {} ../compile-deploy-smartcontract/contracts \;
 
 cd ../compile-deploy-smartcontract/contracts
 npm install
-npm start
+npm start $NODE $PRIVATE_KEY $CONTRACT_ARGS
+exit
