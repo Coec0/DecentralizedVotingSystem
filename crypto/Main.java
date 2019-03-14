078f7538c5c454a12c9b6a0fb6296d59163db2ac
96kevrud <kevinleerudnick@gmail.com>
2019-03-14 13:21:38 +0100

    Fix bug where duplicates would be generated

--- crypto/Main.java
+++ crypto/Main.java
@@ -1,13 +1,22 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     public static void main (String[] args){
-        CyclicGenerator cg = new CyclicGenerator(1,10000);
+        CyclicGenerator cg = new CyclicGenerator(100000,10000000);
         cg.generate();
         System.out.println(Arrays.toString(cg.getGroup()));
         System.out.println("p: " + cg.getP());
         System.out.println("q: " + cg.getQ());
+        System.out.println("g: " + cg.getG());
+        long[] arr = cg.getGroup();
+        Set<Long> s = new HashSet<Long>();
+        for(int i = 0; i < arr.length; i++){
+            s.add(arr[i]);
+        }
+        System.out.println("has no dup?: " + (arr.length == s.size() ? "true" : "false"));
 
     }
 
