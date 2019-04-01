0e86d757ee4e34e71a548b31a6232be088519f37
Zack Lundqvist <Zack.lundqvist@hotmail.com>
2019-04-01 21:13:45 +0200

    Implement EEA instead of fermats

--- client/src/utils/crypto.js
+++ client/src/utils/crypto.js
@@ -4,7 +4,9 @@ let P = { x: 1, y: 60 }
 let Q = { x: 6, y: 21 }
 let m = 109;
 
-console.log(pointAddition(P, Q, m));
+console.time('EEA');
+console.log(modularInverse(3453453453453451, 12312312));
+console.timeEnd('EEA');
 
 function pointAddition(P, Q, m) {
 	const add = function add(P, Q) {
@@ -16,7 +18,6 @@ function pointAddition(P, Q, m) {
 		
 	}
 
-	// Check if identity
 	const firstThing = P.x === Q.x
 	const otherThing = Q.y === mod(-1*P.y, m);
 
@@ -47,10 +48,37 @@ function mod(x, n) {
 	return (x % n + n) % n;
 } 
 
-// This uses eulers theorem to compute the inverse
-// https://en.wikipedia.org/wiki/Modular_multiplicative_inverse#Using_Euler's_theorem
-function modularInverse(a, p) {
-	return mod(Math.pow(a, p - 2), p);
+// Use EEA to compute modular inverse
+// https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+function modularInverse(a, m) {
+	// Validate input
+	[a, m] = [Number(a), Number(m)];
+	if (Number.isNaN(a) || Number.isNaN(m)) {
+		return NaN;
+	}
+	a = mod(a, m);
+	if (!a || m < 2) {
+		return NaN;
+	}
+
+	// Find GCD
+	const s = [];
+	let b = m;
+	while(b) {
+		[a, b] = [b, a % b];
+		s.push({a, b});
+	}
+	if (a !== 1) {
+		return NaN; // inverse does not exists
+	}
+	// find the inverse
+	let x = 1;
+	let y = 0;
+	for(let i = s.length - 2; i >= 0; --i) {
+		[x, y] = [y,  x - y * Math.floor(s[i].a / s[i].b)];
+	}
+
+	return mod(y, m);
 }
 
 module.exports = { pointAddition }
