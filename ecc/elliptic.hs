67e2ee7b6644476a820abbd063d90cf0a3078c97
Sarah Lindau <lsarah@student.chalmers.se>
2019-04-02 14:14:58 +0200

    Improve getGenerators

--- ecc/elliptic.hs
+++ ecc/elliptic.hs
@@ -113,9 +113,11 @@ modExp b e m = t * modExp ((b * b) `mod` m) (shiftR e 1) m `mod` m
 
 -- generate possible generators
 getGenerators :: Curve -> [Point]
-getGenerators c = filter (isPrime . getOrderOfGenerator c) allPoints
+getGenerators c = map snd $ filter (\p -> largestOrder == (fst p)) possibleCandidates
     where 
-        allPoints = findPtsOnCurve c
+        allPoints = map (\p -> (getOrderOfGenerator c p,p)) (findPtsOnCurve c)
+        possibleCandidates = filter (\p -> isPrime (fst p)) allPoints
+        largestOrder = maximum $ map (fst) possibleCandidates 
 
 isPrime k = null [ x | x <- [2..k - 1], k `mod` x == 0]
 -- The main program
