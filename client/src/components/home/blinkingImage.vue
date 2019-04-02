dea409091a9ef4eeedbda42d1417e48ed4f7fdc1
Sarah Lindau <lsarah@student.chalmers.se>
2019-04-02 14:14:58 +0200

    Improve getGenerators

---
+++ client/src/components/home/blinkingImage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="show">
+    <p>{{text}}</p>
+    <img v-bind:src="link"></img>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlinkingImage",
+  data(){
+      return {
+          show: true
+      }
+  },
+  created(){
+      setInterval(()=>{
+         // this.show=!this.show;
+      }, 10000)
+  },
+
+  props: {
+    text: String,
+    link: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
