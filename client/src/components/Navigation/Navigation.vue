80f07f9eb3050b222fa7bae675bd382d73e149b3
Zack Lundqvist <Zack.lundqvist@hotmail.com>
2019-04-08 18:21:11 +0200

    Only show tools navbutton when web3 and contracts are loaded

--- client/src/components/Navigation/Navigation.vue
+++ client/src/components/Navigation/Navigation.vue
@@ -6,8 +6,8 @@
 			</router-link>
 		</div>
 		<NavButton class="nav-button hvr-fade" v-for="item in items" :key="item.id" :text="item.name" :to="'/vote/' + item.id.toString()"></NavButton>
-		<NavButton class="nav-button hvr-fade results" text="Results" to="/results"></NavButton>
-		<NavButton class="nav-button hvr-fade" text="Tools" to="/tools"></NavButton>
+		<NavButton v-if="showElectionSpecifics" class="nav-button hvr-fade results" text="Results" to="/results"></NavButton>
+		<NavButton v-if="showElectionSpecifics" class="nav-button hvr-fade" text="Tools" to="/tools"></NavButton>
 	</div>
 </template>
 
@@ -17,6 +17,11 @@ export default {
 	name: 'Navigation',
 	components: {
 		NavButton
+	},
+	computed: {
+		showElectionSpecifics() {
+			return !!this.$store.state.web3 && !!this.$store.state.contracts;
+		}
 	},
 	props: {
 		items: Array
